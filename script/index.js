c0052e6c030178ea0c7d8f08d2e76b9327ba188e
hieutrandes <hieutran16995@gmail.com>
2019-12-18 03:15:36 -0800

    Updates

--- script/index.js
+++ script/index.js
@@ -1,24 +1,28 @@
 	function indexFunction() {
-	$('[data-toggle="tooltip"]').tooltip()   
+		$('[data-toggle="tooltip"]').tooltip()   
 
 		$('#default_button').click(function() {
 			$("#about").css('color', 'black')
 			$(".button").css('color','black')
+			$("#nav li a").css('color', 'black')
 		})
 		$('#button1').click(function() {
 			$("#about").css('color', 'yellow')
 			$(".button").css('color','white')
-
+			$("#nav li a").css('color', 'white')
 		})
 		$('#button2').click(function() {
-			$("#about").css({'color': '#FC9602', "font-weight" : "bold"})
+			$("#about").css({'color': '#0099ff', "font-weight" : "bold"})
 			$(".button").css('color','white')
-
+			$("#nav li a").css('color', '#0099ff')
 		})
-
 		$('#button3').click(function() {
-			$("#about").css({'color': '#F007FF',"font-weight" : "bold"})
+			$("#about").css({'color': '#00ff99',"font-weight" : "bold"})
+			$("#nav li a").css('color', '#00ff99')
 			$(".button").css('color','white')
-
 		})
+		$(".main-containter.collapse").on('shown.bs.collapse', function () {    
+			$(".main-containter.collapse").not($(this)).collapse('hide');
+		});
+
 	}
